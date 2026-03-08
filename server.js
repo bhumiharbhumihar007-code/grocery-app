94c543a049b60c971eb0d3bf70617cd2139811a8
bhumiharbhumihar007-code <bhumiharbhumihar007@gmail.com>
2026-03-08 22:09:11 +0530

    Remove server start comment and app.listen function
    Removed server start comment and listen function.

--- server.js
+++ server.js
@@ -80,11 +80,3 @@ app.get("/adminData", async (req, res) => {
 
   res.json({ users, orders });
 });
-
-/* =========================
-   Start Server
-========================= */
-
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000 🚀");
-});

9a86d5e99d490832f27df5349c61296d6bd278b0
bhumiharbhumihar007-code <bhumiharbhumihar007@gmail.com>
2026-03-09 13:34:03 +0530

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
     name: { type: String, required: true },
     price: { type: Number, required: true },
     category: { type: String, required: true },
-    image: { type: String, required: true }, // URL of the image
-    description: String,
-    stock: { type: Number, default: 10 }
+    image: { type: String, required: true }, // URL link for the image
+    description: { type: String },
+    date: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('Product', productSchema);
+module.exports = mongoose.model("Product", productSchema);

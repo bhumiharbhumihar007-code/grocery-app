74666816907891d74c74c14b271825eb0248068b
bhumiharbhumihar007-code <bhumiharbhumihar007@gmail.com>
2026-03-11 11:00:16 +0530

    Update login.js

--- public/login.js
+++ public/login.js
@@ -3,27 +3,19 @@ document.getElementById("login-form").addEventListener("submit", async (e)=>{
 e.preventDefault();
 
 const email = document.getElementById("email").value;
-
 const password = document.getElementById("password").value;
 
-
-const response = await fetch("/login",{
-
+const res = await fetch("/login",{
 method:"POST",
-
 headers:{
 "Content-Type":"application/json"
 },
-
 body: JSON.stringify({email,password})
-
 });
 
+const data = await res.json();
 
-const data = await response.json();
-
-
-if(response.ok){
+if(res.ok){
 
 localStorage.setItem("userName",data.userName);
 localStorage.setItem("userEmail",data.userEmail);

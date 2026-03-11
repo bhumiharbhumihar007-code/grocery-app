ea0f614866320890b3e7ae512fa1c74718bc860b
bhumiharbhumihar007-code <bhumiharbhumihar007@gmail.com>
2026-03-11 10:11:01 +0530

    Refactor productSchema for better readability

--- models/Product.js
+++ models/Product.js
@@ -1,12 +1,34 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    price: { type: Number, required: true },
-    category: { type: String, required: true },
-    image: { type: String, required: true }, // URL link for the image
-    description: { type: String },
-    date: { type: Date, default: Date.now }
+    name: { 
+        type: String, 
+        required: true 
+    },
+
+    price: { 
+        type: Number, 
+        required: true 
+    },
+
+    category: { 
+        type: String, 
+        required: true 
+    },
+
+    image: { 
+        type: String, 
+        required: true 
+    }, // URL of medicine image
+
+    description: { 
+        type: String 
+    },
+
+    date: { 
+        type: Date, 
+        default: Date.now 
+    }
 });
 
 module.exports = mongoose.model("Product", productSchema);

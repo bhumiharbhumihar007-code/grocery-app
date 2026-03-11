92bf0d8947c92253f926252f9ba84171ed9e9b8c
bhumiharbhumihar007-code <bhumiharbhumihar007@gmail.com>
2026-03-11 10:50:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,6 +17,11 @@ const app = express();
 
 app.use(express.json());
 app.use(express.static("public"));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+res.sendFile(path.join(__dirname, "public", "login.html"));
+});
 
 
 /* =========================

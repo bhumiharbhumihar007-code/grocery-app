20c8524ce6bfed59d1b4183e0365f9e08b34b4d4
bhumiharbhumihar007-code <bhumiharbhumihar007@gmail.com>
2026-03-11 10:18:02 +0530

    Enhance product schema with new fields and options

--- models/Product.js
+++ models/Product.js
@@ -1,34 +1,50 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-    name: { 
-        type: String, 
-        required: true 
+const productSchema = new mongoose.Schema(
+{
+    name: {
+        type: String,
+        required: true,
+        trim: true
     },
 
-    price: { 
-        type: Number, 
-        required: true 
+    description: {
+        type: String,
+        required: true
     },
 
-    category: { 
-        type: String, 
-        required: true 
+    price: {
+        type: Number,
+        required: true,
+        min: 0
     },
 
-    image: { 
-        type: String, 
-        required: true 
-    }, // URL of medicine image
-
-    description: { 
-        type: String 
+    category: {
+        type: String,
+        required: true
     },
 
-    date: { 
-        type: Date, 
-        default: Date.now 
+    stock: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+
+    image: {
+        type: String
+    },
+
+    brand: {
+        type: String
+    },
+
+    isAvailable: {
+        type: Boolean,
+        default: true
     }
-});
+
+},
+{ timestamps: true }
+);
 
 module.exports = mongoose.model("Product", productSchema);

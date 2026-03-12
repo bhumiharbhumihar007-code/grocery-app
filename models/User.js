18a53a4b8b4efc954bc1688af9680d262a4221f8
bhumiharbhumihar007-code <bhumiharbhumihar007@gmail.com>
2026-03-12 19:11:14 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,12 +3,15 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
     name: { 
         type: String, 
-        required: true 
+        required: true,
+        trim: true
     },
     email: { 
         type: String, 
         required: true, 
-        unique: true 
+        unique: true,
+        lowercase: true,
+        trim: true
     },
     password: { 
         type: String, 

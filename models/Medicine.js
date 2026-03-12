8e7272376d8afa6cf72147bb044295a97704fec2
bhumiharbhumihar007-code <bhumiharbhumihar007@gmail.com>
2026-03-12 17:40:18 +0530

    Update and rename Product.js to Medicine.js

---
+++ models/Medicine.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const MedicineSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    genericName: { type: String },
+    manufacturer: { type: String },
+    price: { type: Number, required: true },
+    category: { type: String, required: true },
+    image: { type: String, default: "/images/default-medicine.jpg" },
+    description: { type: String },
+    stock: { type: Number, default: 0 },
+    expiryDate: { type: Date },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Medicine", MedicineSchema);
